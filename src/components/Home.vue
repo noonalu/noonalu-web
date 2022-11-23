da4774b94334e40cd85459c93f0759a0b4cde10c
Peter Kos <pkos91@icloud.com>
2022-11-23 16:10:15 -0500

    start of styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,37 @@
 </script>
 
 <template>
-	<h1>Home</h1>
+	<div id="header">
+		<div id="header-bg"></div>
+		<h1>Noonalu</h1>
+		<p>Schedule things, probably.</p>
+	</div>
+
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+	#header {
+		margin: 4rem auto 0 auto;
+		padding: 2rem 0;
+		width: 20rem;
+
+		#header-bg {
+			background-color: #FC7753;
+			height: 50px;
+			border-radius: 10px;
+			margin-bottom: 1rem;
+			margin-left: -25%;
+			width: 50%;
+
+		}
+
+		h1 {
+			margin: 0;
+		}
+
+		h1, p {
+			text-align: center;
+		}
+	}
 </style>
