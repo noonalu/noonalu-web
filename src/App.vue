8e93ff10ab6f4dfafe8551049f20dea708314f22
Peter Kos <pkos91@icloud.com>
2022-11-23 14:59:43 -0500

    reindent and cleanup of boilerplate vite stuff

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+	import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
 	<!-- Root view of our app -->
-	<p>
-		<router-link to="/">Go to Home (landing)</router-link>
-		<router-link to="/cal">Go to Calendar</router-link>
-	</p>
-	<router-view></router-view>
+	<HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
