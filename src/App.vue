8cda1beccb7239bfbbc089643bb46450728665be
Peter Kos <pkos91@icloud.com>
2022-11-23 15:34:23 -0500

    basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-	import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
 	<!-- Root view of our app -->
-	<HelloWorld msg="Vite + Vue" />
+	<p>
+		<router-link to="/">Go to Home (landing)</router-link>
+		<router-link to="/cal">Go to Calendar</router-link>
+	</p>
+	<router-view></router-view>
 </template>
 
 <style scoped>
