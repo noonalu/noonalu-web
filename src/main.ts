05d03df33f98bda5d575871137a3d728e11f8719
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2022-11-23 23:07:17 -0500

    [Deployment] stage -> main (#9)
    * reindent and cleanup of boilerplate vite stuff
    
    * basic routing
    
    * [NLU-5] Adds production deployment script (#8)
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+// Components
+import Home from "./components/Home.vue"
+import Calendar from "./components/Calendar.vue"
+
+// Routing
+const routes = [
+	{ path: '/', component: Home },
+	{ path: '/cal', component: Calendar },
+]
+
+const router = createRouter({
+	history: createWebHashHistory(),
+	routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
