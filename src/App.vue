a2450fcb541ee769cbda4c0ea65a1229c3176c96
Peter Kos <pkos91@icloud.com>
2022-11-25 17:21:42 -0500

    [Deployment] stage -> main (#13)
    * reindent and cleanup of boilerplate vite stuff
    
    * basic routing
    
    * [NLU-2] Landing page | Actual site and stuff, with placeholder content (#10)
    
    * add sass
    
    * start of styling
    
    * wow
    
    * Content stuff in the middle but no images
    
    * Footer progress
    
    * footer done
    
    * add content
    
    * Final responsive and sub tweaks
    
    * [NLU-4] Removes GitHub Actions for Netlify deployments (#12)
    
    Co-authored-by: Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 
 <template>
 	<!-- Root view of our app -->
-	<p>
-		<router-link to="/">Go to Home (landing)</router-link>
-		<router-link to="/cal">Go to Calendar</router-link>
-	</p>
+	<!-- <p> -->
+		<!-- <router-link to="/">Go to Home (landing)</router-link> -->
+		<!-- <router-link to="/cal">Go to Calendar</router-link> -->
+	<!-- </p> -->
 	<router-view></router-view>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './styles/main.scss'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
