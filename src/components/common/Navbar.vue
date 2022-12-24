9308f09890a852ff673239cf7f02603429c82620
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2022-12-23 23:43:52 -0500

    Refactors header and footer (#17)
    * [Deployment] stage -> main (#13)
    
    * reindent and cleanup of boilerplate vite stuff
    
    * basic routing
    
    * [NLU-2] Landing page | Actual site and stuff, with placeholder content (#10)
    
    * add sass
    
    * start of styling
    
    * wow
    
    * Content stuff in the middle but no images
    
    * Footer progress
    
    * footer done
    
    * add content
    
    * Final responsive and sub tweaks
    
    * [NLU-4] Removes GitHub Actions for Netlify deployments (#12)
    
    Co-authored-by: Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
    
    * Refactors header and footer
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<nav>
+		<div id="links">
+			<a href="#">features</a>
+			<a href="#">pricing</a>
+			<a href="#">about</a>
+		</div>
+	</nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+		display: inline-block;
+		width: 100%;
+		display: flex;
+		justify-content: flex-end;
+
+		#links {
+			margin-right: 2rem;
+			display: flex;
+		}
+
+		a {
+			text-decoration: none;
+			color: black;
+			margin: 20px;
+			font-size: 1.5rem;
+			font-family: 'Varela Round', sans-serif;
+			transition: 0.1s all;
+			border-bottom: 3px solid transparent;
+
+			&:hover {
+				transform: translateY(-3px);
+				color: #FC7753;
+				border-bottom: 3px solid #FC7753;
+			}
+		}
+	}
+</style>
